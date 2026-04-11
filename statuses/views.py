139d65ad5c4d687505e1b20ba3923365103b5b23
Ferman <nferman01@gmail.com>
2026-04-11 16:34:24 +0300

    statuses

--- statuses/views.py
+++ statuses/views.py
@@ -4,6 +4,8 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import ProtectedError
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.shortcuts import redirect
+from httpx import request
 
 from .forms import StatusForm
 from .models import Status
@@ -38,6 +40,11 @@ class StatusDeleteView(LoginRequiredMixin, DeleteView):
     template_name = 'statuses/delete.html'
     success_url = reverse_lazy('statuses')
 
-    def form_valid(self, form):
-        messages.success(self.request, 'Статус успешно удален')
-        return super().form_valid(form)
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        try:
+            self.object.delete()
+            messages.success(request, 'Статус успешно удален')
+        except ProtectedError:
+            messages.error(request, 'Невозможно удалить статус')
+        return redirect(self.success_url)
