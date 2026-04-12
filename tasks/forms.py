02ff5e06bc6fa210189e225e2dc457a066df400f
Ferman <nferman01@gmail.com>
2026-04-12 12:29:43 +0300

    32

--- tasks/forms.py
+++ tasks/forms.py
@@ -5,10 +5,10 @@ from .models import Task
 
 class TaskForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
-    super().__init__(*args, **kwargs)
-    self.fields['executor'].label_from_instance = (
+        super().__init__(*args, **kwargs)
+        self.fields['executor'].label_from_instance = (
         lambda user: user.get_full_name() or user.username
-    )
+        )
     class Meta:
         model = Task
         fields = ('name', 'description', 'status', 'executor', 'labels')
