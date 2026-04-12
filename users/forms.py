ed65aa70fe37bea976916f58fb395a2d634c1efd
Ferman <nferman01@gmail.com>
2026-04-12 12:45:03 +0300

    34

--- users/forms.py
+++ users/forms.py
@@ -27,10 +27,35 @@ class UserUpdateForm(forms.ModelForm):
     first_name = forms.CharField(label='Имя', max_length=150, required=False)
     last_name = forms.CharField(label='Фамилия', max_length=150, required=False)
     username = forms.CharField(label='Имя пользователя', max_length=150)
+    password1 = forms.CharField(label='Пароль', required=False, widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Подтверждение пароля', required=False, widget=forms.PasswordInput)
 
     class Meta:
         model = User
-        fields = ('first_name', 'last_name', 'username')
+        fields = ('first_name', 'last_name', 'username', 'password1', 'password2')
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password1 = cleaned_data.get('password1')
+        password2 = cleaned_data.get('password2')
+
+        if password1 or password2:
+            if password1 != password2:
+                self.add_error('password2', 'Пароли не совпадают')
+
+        return cleaned_data
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        password = self.cleaned_data.get('password1')
+
+        if password:
+            user.set_password(password)
+
+        if commit:
+            user.save()
+
+        return user
 
 class UserLoginForm(AuthenticationForm):
     def __init__(self, *args, **kwargs):
